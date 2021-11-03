696f7dedab56db7a31395e75c2a190d7913bb872
FarukOZT <92529449+FarukOZT@users.noreply.github.com>
2021-11-03 23:02:42 +0300

    Add files via upload

--- demo/src/main/java/com/sampiyonlarligi/memberController/MemberController.java
+++ demo/src/main/java/com/sampiyonlarligi/memberController/MemberController.java
@@ -1,38 +1,45 @@
 package com.sampiyonlarligi.memberController;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sampiyonlarligi.member.Members;
 import com.sampiyonlarligi.service.Services;
 
 @RestController
-
+@RequestMapping("/members")
 public class MemberController {
 	
 	@Autowired
 	public Services services;
 	
-	@GetMapping("/members")
-	public List<Members> all_members() {
-		return services.getAllMemberList();
+	@GetMapping
+	public ResponseEntity<Members> getAllMemberList() {
+		return new ResponseEntity(services.getAllMemberList(),HttpStatus.OK);
 	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Members> getAllMemberList(@PathVariable("id") UUID id){
+		return new ResponseEntity(services.getMember(id),HttpStatus.OK);
+	}
+	
 	@PostMapping("/add")
-		public void addMembers(@RequestBody Members members) {
-		services.addMembers(members);
+		public ResponseEntity<Members> addMembers(@RequestBody Members members) {
+		return new ResponseEntity(services.addMembers(members),HttpStatus.OK);
 	}
+	
 	@DeleteMapping("/{id}")
 		public void deleteMembers(@PathVariable("id") UUID id) {
-		services.deleteMembers(id);
-		
+		services.deleteMembers(id);	
 	}
 	
 }
--- demo/src/main/java/com/sampiyonlarligi/service/Services.java
+++ demo/src/main/java/com/sampiyonlarligi/service/Services.java
@@ -1,31 +1,18 @@
 package com.sampiyonlarligi.service;
 
-import java.util.*;
-
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.sampiyonlarligi.member.Members;
 
-@Service
-public class Services {
+public interface Services {
 	
-	public List<Members> 
-	memberList = new ArrayList<>(Arrays.asList());
-	
-	public List<Members> getAllMemberList() {
-		return memberList;
-	}
-	public void addMembers(Members members) {
-		UUID id = UUID.randomUUID();
-		members.setId(id);
-		
-		memberList.add(members);
-	
-	}
-	public void deleteMembers(UUID id) {
-		
-		memberList.removeIf(m -> m.getId().equals(id));
-	}
-
+	Members addMembers(Members members);
+	void deleteMembers(UUID id);
+	List<Members> getAllMemberList();
+	Optional<Members> getMember(UUID id);
 }
+	
+	
 
